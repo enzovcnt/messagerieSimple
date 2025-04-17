7d695d26bf7aaa3a85a3981d311ecd8782579aec
Enzo <enzovcnt@gmail.com>
2025-04-17 18:55:10 +0200

    create message with success with a form

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
+use App\Form\MessageType;
 use App\Repository\MessageRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,6 +19,28 @@ final class MessageController extends AbstractController
 
         return $this->render('message/index.html.twig', [
             'messages' => $repository->findAll(),
+        ]);
+    }
+
+    #[Route('/create', name: 'create_message')]
+    public function create(Request $request, EntityManagerInterface $manager): Response
+    {
+        $message = new Message();
+
+        $formMessage = $this->createForm(MessageType::class, $message);
+
+        $formMessage->handleRequest($request);
+        if ($formMessage->isSubmitted() && $formMessage->isValid())
+        {
+            $message->setCreatedAt(new \DateTime());
+            $manager->persist($message);
+            $manager->flush();
+            return $this->redirectToRoute('messages');
+        }
+
+
+        return $this->render('message/create.html.twig', [
+            'formMessage' => $formMessage->createView(),
         ]);
     }
 }
